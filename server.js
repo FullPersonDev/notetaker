708940f4915850cc73a5dd1acc13d2cae5a24c30
Martin Aguero <martin.aguero84@gmail.com>
2024-01-25 21:31:46 -0600

    added middleware to parse incoming data as json

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 // Add required packages
-const exp = require('constants');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const database = require('./db/db.json');
+const { stringify } = require('querystring');
+
 // Create Port
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 // Create app
 const app = express();
 
 // Middleware to use public folder
 app.use(express.static('public'));
 
-// Set routes
+// Middleware to parse incoming data as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// Set routes for landing page and notes page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// Set routes for get and post apis to the DB
+app.get('/api/notes', (req, res) => {
+    res.json(database);
+});
+app.post('/api/notes', (req, res) => {
+    res.json(database);
 });
 
 // Create app listner
